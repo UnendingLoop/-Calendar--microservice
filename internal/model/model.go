0631d9d66a8ed9955f619fa9b30530ee8535ee5c
FANIL KAMA <1618396fkam@gmail.com>
2026-03-18 15:43:10 +0100

    Added: support for local time

--- internal/model/model.go
+++ internal/model/model.go
@@ -13,13 +13,13 @@ type CustomTime struct {
 }
 
 type Event struct {
-	EID         string      `json:"event_id"`                             // id задачи/события
-	UID         uint        `json:"user_id,omitempty" binding:"required"` // id создателя задачи
-	Created     time.Time   `json:"created"`                              // дата создания задачи/события в UTC, для внутреннего использования
-	Updated     time.Time   `json:"updated"`                              // дата обновления события/задачи пользователем в UTC, для внутреннего использования
-	Scheduled   *CustomTime `json:"date"`                                 // дата выполнения/наступления задачи/события
-	Description string      `json:"event"`                                // сам текст события/задачи
-	IsDone      bool        `json:"is_done"`                              // флаг "отправленности"/выполненности задачи/события
+	EID         string      `json:"event_id"`                   // id задачи/события
+	UID         uint        `json:"user_id" binding:"required"` // id создателя задачи
+	Created     time.Time   `json:"created"`                    // дата создания задачи/события в UTC, для внутреннего использования
+	Updated     time.Time   `json:"updated"`                    // дата обновления события/задачи пользователем в UTC, для внутреннего использования
+	Scheduled   *CustomTime `json:"date" binding:"required"`    // дата выполнения/наступления задачи/события
+	Description string      `json:"event" binding:"required"`   // сам текст события/задачи
+	IsDone      bool        `json:"is_done"`                    // флаг "отправленности"/выполненности задачи/события
 }
 
 func (ct *CustomTime) UnmarshalJSON(b []byte) error {
@@ -28,16 +28,20 @@ func (ct *CustomTime) UnmarshalJSON(b []byte) error {
 		ct.Time = time.Time{}
 		return nil
 	}
-	t, err := time.Parse("2006-01-02", s)
+
+	loc := time.Local
+
+	t, err := time.ParseInLocation("2006-01-02 15:04", s, loc)
 	if err != nil {
 		return err
 	}
+
 	ct.Time = t.UTC()
 	return nil
 }
 
 func (ct *CustomTime) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + ct.Time.Format("2006-01-02") + `"`), nil
+	return []byte(`"` + ct.Time.Local().Format("2006-01-02 15:04") + `"`), nil
 }
 
 type EventRecord struct {
